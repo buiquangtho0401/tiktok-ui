5d89fa9e2d6c77d36d8fcfb291e17a151ddc659d
Your Name <you@example.com>
2022-05-12 15:51:46 +0700

    cau hinh routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,39 @@
+import { Fragment } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { publicRoutes } from '~/routes';
+import { DefaultLayout } from './components/Layout';
 function App() {
     return (
-        <div className="App">
-            <h1>react app</h1>
-        </div>
+        <Router>
+            <div className="App">
+                <Routes>
+                    {publicRoutes.map((route, index) => {
+                        //nếu mà layoout bằng null thì fragment conf nguowcj lai thi lay defau
+                        // const Layout = route.layout === null ? Fragment : DefaultLayout;
+                        const Page = route.component;
+                        let Layout = DefaultLayout;
+                        if (route.layout) {
+                            // Fragment la k co layout
+                            Layout = route.layout;
+                        } else if (route.layout === null) {
+                            Layout = Fragment;
+                        }
+                        console.log(route.layout);
+                        return (
+                            <Route
+                                key={index}
+                                path={route.path}
+                                element={
+                                    <Layout>
+                                        <Page />
+                                    </Layout>
+                                }
+                            />
+                        );
+                    })}
+                </Routes>
+            </div>
+        </Router>
     );
 }
 
