4e1b26df0fa5cea739a98b256caf18c97e4eef36
Your Name <you@example.com>
2022-05-13 14:29:51 +0700

    header và thư viện tippyjs

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,4 +1,29 @@
+import classNames from 'classnames/bind';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import styles from './Header.module.scss';
+import images from '~/assets/images';
+import { faCircleXmark, faSpinner, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+const cx = classNames.bind(styles);
 function Header() {
-    return <h2>header</h2>;
+    return (
+        <header className={cx('warpper')}>
+            <div className={cx('inner')}>
+                <div className={cx('logo')}>
+                    <img src={images.logo} alt="tiktok" />
+                </div>
+                <div className={cx('search')}>
+                    <input className="" placeholder="search account and videos" spellCheck={false} />
+                    <button className={cx('clear')}>
+                        <FontAwesomeIcon icon={faCircleXmark} />
+                    </button>
+                    <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+                    <button className={cx('search-btn')}>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </button>
+                </div>
+                <div className={cx('action')}></div>
+            </div>
+        </header>
+    );
 }
 export default Header;
